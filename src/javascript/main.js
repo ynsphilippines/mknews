68e889adacec3ddf678c85a4c86eb46517daa6b7
cathdionisio <macatherinedionisio@gmail.com>
2020-07-07 13:36:56 +0800

    MN-487 use test email

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -380,13 +380,13 @@ function handleSendMessage( value ) {
     let validationStatus;
     const emailSender      = 'sendonly@y-n-s.co.jp';
     const emailReceiver    = 'issue-SMN-bx7EwRrfyy8Z9Wvzu43ANT4Jp@i3.backlog.jp';
-    const emailHost        = 'smtp.elasticemail.com'; 
-    const password         = '8978ADCF6391790FEFAAD019A006F945F6C6'; 
+    //const emailHost        = 'smtp.elasticemail.com';
+    //const password         = '8978ADCF6391790FEFAAD019A006F945F6C6'; 
     const emailAddress     = 'third.party.service.yns@gmail.com'; 
 
     // FOR TESTING
-    //const emailHost        = 'smtp.gmail.com';
-    //const password         = 'ynsadmin1234'; 
+    const emailHost        = 'smtp.gmail.com';
+    const password         = 'ynsadmin1234'; 
 
     let departNameAndSummaryValue = "";
     if ( value['departmentName'] === "") {
